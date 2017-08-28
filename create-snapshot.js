412b9cbe8ac699c1a7673fe0e4d5a0df600614af
Rodgers Tanui <rtankip@gmail.com>
2017-08-28 16:10:49 +0300

    Bump v2.1.0

--- create-snapshot.js
+++ create-snapshot.js
@@ -7,6 +7,12 @@ function main() {
     var cleanVersion = semver.clean(newVersion);
     var nextPatch = semver.inc(cleanVersion, 'patch');
     package.version = nextPatch + '-SNAPSHOT';
+    var cb = execSync('git symbolic-ref --short HEAD').toString().trim();
+    execSync('git checkout master');
+    fs.writeFileSync('package.json', JSON.stringify(package, null, 4));
+    execSync('git add .');
+    execSync(`git commit -m "Master Updated to ${nextPatch} snapshot "`);
+    execSync(`git checkout ${cb}`);
     fs.writeFileSync('package.json', JSON.stringify(package, null, 4));
     execSync(`rm package-snapshot.json`);
     execSync(`git add -A`);
